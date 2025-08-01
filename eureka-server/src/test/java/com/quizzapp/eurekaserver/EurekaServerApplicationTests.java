2fa158ccb6325a710a672559fc26ff42fb24d733
projectcdac2025 <projectcdac2025@gmail.com>
2025-08-01 14:36:23 +0530

    eureka-server

---
+++ eureka-server/src/test/java/com/quizzapp/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizzapp.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
